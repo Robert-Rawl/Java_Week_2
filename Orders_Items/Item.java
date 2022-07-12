9d4816c3fffff9bcdc216bdac20a6ac5d41181ec
Rob_Rawl <34248558+Robert-Rawl@users.noreply.github.com>
2022-07-12 16:19:24 -0500

    Java_Java_Pt2

--- Orders_Items/Item.java
+++ Orders_Items/Item.java
@@ -1,7 +1,23 @@
 //  CLASS Member Variables
 public class Item {
-    public String name;    
-    public double price;
+    private String name;    
+    private double price;
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
     
     
 
--- Orders_Items/Order.java
+++ Orders_Items/Order.java
@@ -1,8 +1,63 @@
 import java.util.ArrayList;
 // CLASS MEMBER VARIABLES
 public class Order {
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Item> items = new ArrayList<Item>();
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items = new ArrayList<Item>();
+
+    public Order(){
+        this.name = "Guest";
+        this.ready = false;
+    }
+    public Order(String name ){
+        this.name = name;
+        this.ready = false;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public boolean getReady(){
+        return this.ready;
+    }
+    public ArrayList<Item> getItems(){
+        return items;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setReady(boolean ready){
+        this.ready = ready;
+    }
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+    //  Add an item to the order Array
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+    //  Get Order Status, return boolean
+    public String getStatusMessage(){
+        if(this.ready == true){
+            return "Your Order Is Ready";
+        }else {
+            return "Thank You for your patience. Your order should be up soon.";
+        }
+    }
+    // Get Order Total add all items and return a total
+    public double getOrderTotal(){
+        double total = 0;
+        for(Item i: this.items){
+            total += i.getPrice();
+        }
+        return total;
+    }
+    // Display info 
+    public void displayInfo(){
+        System.out.println("********************");
+        System.out.printf("Customer Name: %s%n", this.name);
+        for(Item i: this.items) {
+            System.out.println(i.getName() + " - $" + i.getPrice());
+        }
+        System.out.println("Total: $" + this.getOrderTotal());
+    }
 }
